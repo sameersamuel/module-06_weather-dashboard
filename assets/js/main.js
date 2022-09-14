c5c141d9a73ff0848f263c83439fbd1ab57efafc
Sameer B <sameerflyleaf@gmail.com>
2022-09-13 20:45:41 -0700

    changed tables to p tags in html. pulling city, temp, humidity, and wind from weather api. need to get uvi and 5 day forcast. APPEND CITY SEARCH TO PAGE

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,4 +1,9 @@
+// var apiUrlWeather = `https://api.openweathermap.org/data/2.5/weather?appid=${weatherAPIKey}&q=${name}&units=imperial`;
+// var apiUrlWeather = `https://api.openweathermap.org/data/2.5/weather?appid=${weatherAPIKey}&q=${name}&units=imperial`;
+//   get the forecast, uses lat/lon
+// var apiUrlForecast = `https://api.openweathermap.org/data/2.5/forecast?lat=${lat}&lon=${lon}&units=imperial&appid=${weatherAPIKey}`;
 // let UVQueryURL =
+
 //           "https://api.openweathermap.org/data/2.5/onecall?lat=" +
 //           lat +
 //           "&lon=" +
@@ -17,29 +22,31 @@
 var geoCodingUrl = 'http://api.openweathermap.org/geo/1.0/direct?q='
 var sixteen_url = 'api.openweathermap.org/data/2.5/forecast/daily?lat=44.34&lon=10.99&cnt=7&appid={API key}';
 var myApiKey = '73713d092fbc2614296bf3acc8509c6a';
-var oneCall30 = 'https://api.openweathermap.org/data/3.0/onecall?lat=';
+// var oneCall30 = 'https://api.openweathermap.org/data/3.0/onecall?lat='; 
+var forcastUrl = 'https://api.openweathermap.org/data/2.5/forecast?lat=';
 var searchBtn = document.querySelector('#button');
 var inputValueEl = document.querySelector('#inputValue');
 var nameEl = document.querySelector('#name');
-var descEl = document.querySelector('#desc');
+
 var tempEl = document.querySelector('#temp');
+var cityEl = document.querySelector('#city');
+var windspeedEl = document.querySelector('#windspeed');
+var humidityEl = document.querySelector('#humidity');
+
 var lat,lon;
 
 
-// function makeOneCall30 () {
-//   fetch(`${oneCall30}${lat}&lon=${lon}&appid=${myApiKey}`)
-//   .then(response => response.json())
-//       .then(data => {
-//         console.log(data);
-//         lat = data[0].lat;
-//         console.log(lat);
-//         lon = data[0].lon;
-//         console.log(lon);
+function fetchForecast () {
+  fetch(`${forcastUrl}${lat}&lon=${lon}&units=imperial&appid=${myApiKey}`)
+  .then(response => response.json())
+      .then(data => {
+        console.log(data);
+      
         
-//       })
-//       .catch(err => alert('Wrong city name!'))
+      })
+      .catch(err => console.log(err));
 
-// }
+}
 function getCoordinates (city) {
   fetch(`${geoCodingUrl}${city}&appid=${myApiKey}`)
   .then(response => response.json())
@@ -51,31 +58,49 @@ function getCoordinates (city) {
         console.log(lon);
         
       })
-      .catch(err => alert('Wrong city name!'))
+      .catch(err => console.log(err))
 };
 
-function displayCurrentWeather (data) {
+function displayCurrentWeather (city, data) {
 
-  var temp, humidity, city, windSpeed, weatherIcon;
+  var temp, humidity, windSpeed, weatherIcon;
   temp = Math.floor ((data.main.temp- 273.15) * 1.8 +32);
+  tempEl.innerText = "Temp: " + temp + "F" ;
   console.log(temp);
+
+  humidity = data.main.humidity;
+  console.log(data.main);
+  console.log(humidity);
+  humidityEl.innerText = "Humidity: " + humidity + "%";
+  windSpeed = data.wind.speed;
+  windspeedEl.innerText = "Wind: " + windSpeed + "mph";
+  cityEl.innerText = "City: " + city;
+
+
+
 }
 
 button.addEventListener('click', function() {
     //get cooridinates from openweather
     var city = inputValue.value
+    console.log(city);
     getCoordinates (city)
     // makeOneCall30();
-    fetch(`https://api.openweathermap.org/data/2.5/weather?q=${inputValue.value}&appid=${myApiKey}`)
+    fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${myApiKey}`)
       .then(response => response.json())
       .then(data => {
         
         console.log(data)
-        displayCurrentWeather(data)
+        displayCurrentWeather(city, data)
+        fetchForecast ();
+          
+
       })
 
-      .catch(err => alert('Wrong city name!'))
+      .catch(err => console.log(err))
 });
+
+
 
 
 
